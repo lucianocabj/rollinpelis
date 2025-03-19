13084edc458e0f66545f8ca539ee0e9844325bbf
luciano <hlluciano@hotmail.com>
2025-03-19 17:22:56 -0300

    se crea el carrousel y las listas de peliculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage' 
 import { NavbarC } from './components/navbar/NavbarC'
+import CarrouselC from './components/navbar/carrousel/CarrouselC'
+import { Carousel } from 'bootstrap'
+import { CarouselCaption } from 'react-bootstrap'
 // import { LoginPage } from './pages/LoginPage'
 // import { RegisterPage } from './pages/RegisterPage'
 // import { UserPage } from './pages/userPage'
@@ -12,6 +15,7 @@ export const App = () => {
   return (
     <Router>
       <NavbarC/>
+      <CarrouselC/>
       <Routes>
         <Route path='/' element={<HomePage />} />
         {/* <Route path='/user' element={<UserPage />} /> */}
