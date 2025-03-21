7c3219e725f5065a1b40df47828fda1b7af57922
luciano <hlluciano@hotmail.com>
2025-03-21 10:09:20 -0300

    correcion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { NavbarC } from './components/navbar/NavbarC';
-import CarrouselC from './components/navbar/carrousel/CarrouselC'; 
 
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
 import { UserPage } from './pages/UserPage';
 import { AdminPage } from './pages/AdminPage';
-
+import {CarrouselC} from './components/carrousel/CarrouselC';
 function App() {
   return (
     <Router>
