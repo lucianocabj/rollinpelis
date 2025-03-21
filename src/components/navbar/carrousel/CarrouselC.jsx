7c2ed79634940d92ef6f9fc2d4617d85d188b9b5
luciano <hlluciano@hotmail.com>
2025-03-20 21:38:08 -0300

    Se actualizo las imagenes y se guardo el array en localStorage

--- src/components/navbar/carrousel/CarrouselC.jsx
+++ src/components/navbar/carrousel/CarrouselC.jsx
@@ -12,39 +12,41 @@ const CarrouselC = () => {
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
         <img
-          className="w-100 h-50vh"
-          src="https://i.blogs.es/b6837d/halloween/1366_2000.jpg"
+          className="w-100"
+          style={{ height: "50vh", objectFit: "cover" }}
+          src="https://i.ytimg.com/vi/crDNzugmuGY/maxresdefault.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>Terror</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h2>ESTRENOS</h2>
+          <p>IT</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/800x400"
-          alt="asdasdasd"
+          className="w-100"
+          style={{ height: "50vh", objectFit: "cover" }}
+          src="https://encuentra.com/wp-content/uploads/2019/12/Frozen2.encuentra.com_.jpg"
+          alt="Second slide"
         />
         <Carousel.Caption>
-          <h3>sadasdsa</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h2>ESTRENOS</h2>
+          <p>FROZEN</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/800x400"
+          className="w-100"
+          style={{ height: "50vh", objectFit: "cover" }}
+          src="https://scontent.ftuc4-2.fna.fbcdn.net/v/t39.30808-6/475550278_9305740269461757_6230325877676308_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeFlzf59KA1yJNt9reHnycKFDrrJUduSL5sOuslR25Ivm-veDyMUzvT-RaD9KLmvIEeqB7GqWeJ6ro7Wo1vdbGEX&_nc_ohc=s8DF9t8Tl-kQ7kNvgHoRZ_q&_nc_oc=Adn3IpM5ykJupNbbKwjlbR-E8Ucso4zYYylRAvkieN7bPs9JezL97Au9O74qLGr7Nskh7xLLb-MK7KcPiXI3nHUx&_nc_zt=23&_nc_ht=scontent.ftuc4-2.fna&_nc_gid=8C0MR_SfTh2sN1sU5t-Gqg&oh=00_AYHfIOh-gHwNeiBs40IoD_YJWxP5ppwCIcDAtCdNfqjtFA&oe=67E2236A"
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          <h2>ESTRENOS</h2>
+          <p>IMAX</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { App } from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import CarrouselC from './components/navbar/carrousel/CarrouselC'
 
 
 
