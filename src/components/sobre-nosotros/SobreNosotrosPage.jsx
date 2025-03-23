1ca956c92141b24a4c81f57ca4955b298a51f0dd
Silvana Velastiqui <silvana.velastiqui@gmail.com>
2025-03-23 15:37:16 -0300

    creacion de rama sobre nosotros

---
+++ src/components/sobre-nosotros/SobreNosotrosPage.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const SobreNosotrosPage = () => {
+  return (
+    <>
+      <div>
+        <h1> Sobre Nosotros </h1>
+
+        <h3>
+          Somos estudiantes de la carrera FullStack del instituto RollinCode. "
+          El unico modo de hacer un gran trabajo, es amar lo que se hace "
+          Steven Jobs
+        </h3>
+      </div>
+      <div className="imgequipo">
+        <img
+          src="blob:https://web.whatsapp.com/5832aa01-76ce-4235-a28b-be7578fa5243 "
+          alt="Facundo "
+        />
+        <img
+          src="https://media-eze1-1.cdn.whatsapp.net/v/t61.24694-24/473397673_620100887262920_7695228157224992913_n.jpg?ccb=11-4&oh=01_Q5AaIVnRrKWuREO3j8LoJ60Vkv5FbzAn0sJIFKum_xDeCOkO&oe=67ECF24F&_nc_sid=5e03e0&_nc_cat=108 "
+          alt="Luciano "
+        />
+        <img
+          src="https://media-eze1-1.cdn.whatsapp.net/v/t61.24694-24/475801669_2072489126492991_5185725608220141821_n.jpg?ccb=11-4&oh=01_Q5AaIT_4Bp6ie8B9UmBY7QBnGp6P2hmFI5JIe4lLbUV42eCu&oe=67ECEF4E&_nc_sid=5e03e0&_nc_cat=103 "
+          alt="Silvana "
+        />
+      </div>
+    </>
+  );
+};
+
+export default SobreNosotrosPage;
